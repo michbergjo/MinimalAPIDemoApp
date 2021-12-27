848171bb20f6e5789d7f51e63be9cfeb76ebff38
Michelle Joseph <mbergy27@gmail.com>
2021-12-27 12:52:05 -0600

    Initial commit

---
+++ DataAccess/Data/UserData.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.DbAccess;
+using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data;
+
+public class UserData
+{
+    private readonly ISqlDataAccess _db;
+ 
+
+    public UserData(ISqlDataAccess db)
+    {
+        _db = db;
+    }
+
+    public Task<IEnumerable<UserModel>> GetUser() => _db.LoadData<UserModel, dynamic>(storedProcedure: "dbo.spUser_GetAll", new {});
+}
